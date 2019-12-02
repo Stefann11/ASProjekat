0dee7fd3c90949f9fcc133b0a6aa671e36708191
User <User@DESKTOP-4IJKDER>
2019-12-02 12:31:35 +0100

    Uradio brisanje studenta (treba jos dodati samo brisanje iz tabele)

--- src/view/BrisanjeStudenta.java
+++ src/view/BrisanjeStudenta.java
@@ -1,17 +1,32 @@
 package view;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class BrisanjeStudenta extends JOptionPane{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public BrisanjeStudenta() {
 		ImageIcon icon = new ImageIcon("images/trash-icon.png");
-        JOptionPane.showMessageDialog(
+		UIManager.put("OptionPane.yesButtonText", "Potvrda");
+
+		UIManager.put("OptionPane.noButtonText", "Odustanak");
+		int choice =JOptionPane.showConfirmDialog(
                 null,
-                "Da li ste sigurni da želite da obrišete studenta",
-                "Brisanje Studenta", JOptionPane.INFORMATION_MESSAGE,icon);
-        
+                "Da li ste sigurni da želite da obrišete studenta?",
+                "Brisanje Studenta",YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE,icon);
+		if (choice == JOptionPane.YES_OPTION) {
+
+			JOptionPane.showMessageDialog(null, "Student obrisan!");
+		} else {
+
+			JOptionPane.showMessageDialog(null, "Student nije obrisan.");
+
+			}
 	}
 	
 }
