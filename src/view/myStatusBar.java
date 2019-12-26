0ec81372e7b905b3e13cfa0015aae273a7ec5337
User <User@DESKTOP-4IJKDER>
2019-12-26 15:06:04 +0100

    Status bar ispravljen

--- src/view/myStatusBar.java
+++ src/view/myStatusBar.java
@@ -16,7 +16,7 @@ public class myStatusBar extends JPanel{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 5260586352929905286L;
 	
 
 	
@@ -24,25 +24,25 @@ public class myStatusBar extends JPanel{
         super();
         setLayout(new BorderLayout());
         
-        Label studentska_sluzba=new Label("Studentska sluzba");
+        Label studentskaSluzba=new Label("Studentska sluzba");
         
-        Date date2=new Date();
-        SimpleDateFormat datum_format = new SimpleDateFormat("HH:mm  dd.MM.yyyy. ");
+   
+        SimpleDateFormat datumFormat = new SimpleDateFormat("HH:mm  dd.MM.yyyy. ");
         Label timedate=new Label();
         
         Timer timer = new Timer(1000, new ActionListener() {
       		@Override
 			public void actionPerformed(ActionEvent evt) {
 				Date date = new Date();
-        		timedate.setText(datum_format.format(date));
+        		timedate.setText(datumFormat.format(date));
 			}
         });
         
         timer.start();
         timedate.setPreferredSize(new Dimension(110, 10));
         this.add(timedate, BorderLayout.EAST);
-        this.add(studentska_sluzba, BorderLayout.WEST);
-       // this.setPreferredSize(new Dimension(1, 19));
+        this.add(studentskaSluzba, BorderLayout.WEST);
+   
         
         
 	}
