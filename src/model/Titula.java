2c0dce0b637078825cce3a59574e3e56c3aa0970
Stefann11 <santrac.stefan@gmail.com>
2020-01-09 12:25:38 +0100

    Profesor- promena enuma titula i zvanje

--- src/model/Titula.java
+++ src/model/Titula.java
@@ -1,6 +1,17 @@
 package model;
 
 public enum Titula{
-	MAGISTAR, DOKTOR; 
+	PROF_DR("Prof dr"), DR("Dr"); 
+	
+	 private String displayName;
+
+	Titula(String displayName) {
+        this.displayName = displayName;
+    }
+	
+	public String displayName() { return displayName; }
+
+    // Optionally and/or additionally, toString.
+    @Override public String toString() { return displayName; }
 
 }
--- src/model/Zvanje.java
+++ src/model/Zvanje.java
@@ -1,5 +1,16 @@
 package model;
 
 public enum Zvanje {
-	ASISTENT, SARADNIK, PROFESOR, DEKAN;
+	REDOVNI_PROCESOR("Redovni profesor"), VANDREDNI_PROCESOR("Vandredni profesor"), DOCENT("Docent");
+
+	private String displayName;
+
+	Zvanje(String displayName) {
+        this.displayName = displayName;
+    }
+	
+	public String displayName() { return displayName; }
+
+    // Optionally and/or additionally, toString.
+    @Override public String toString() { return displayName; }
 }
